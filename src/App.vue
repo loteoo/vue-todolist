b29921f804ea68cb95f9050a901bd7714246a2ed
Alexandre Lotte <alexlotte16@gmail.com>
2018-10-28 01:37:52 -0400

    todo list

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,162 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <div class="card">
+      <div class="left">
+        <h1>Vue.js test app</h1>
+        <p>
+          <a href="https://github.com/loteoo/vue-todolist" target="_blank">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-github"><path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path></svg>
+            Source code
+          </a>
+        </p>
+      </div>
+      <div class="right">
+        <TodoList />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TodoList from './components/TodoList.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    TodoList
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+
+/* Sane css reset */
+@import '~sanitize.css';
+
+/*------------------------------------*\
+    Global css
+\*------------------------------------*/
+
+html {
+	--text-color: #FFFFFF;
+	--background-color: #323232;
+	--accent-color: #06D19C;
+  width: 100%;
+  height: 100%;
+  font-family: 'CircularStd', 'Helvetica Neue', Helvetica, Arial, sans-serif;
+  font-size: 16px;
+  color: var(--text-color);
+  background-color: var(--background-color);
+  text-rendering: optimizeLegibility;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  scroll-behavior: smooth;
 }
+body {
+  position: relative;
+  overflow-x: hidden;
+}
+
+
+.container {
+  min-height: calc(100vh - 3rem);
+  padding: 15vh 1rem;
+  width: 100%;
+  max-width: 992px;
+  margin: 0 auto;
+}
+
+@media (max-width: 768px) {
+  .container {
+    padding: 0;
+  }
+}
+
+
+a {
+  color: inherit;
+  cursor: pointer;
+  text-decoration: underline;
+}
+a:hover {
+  color: var(--accent-color);
+}
+
+button, input, select, textarea {
+  background-color: transparent;
+  border: none;
+  color: inherit;
+}
+
+input[type="text"] {
+  width: 100%;
+  padding: 0.5rem 1rem;
+  background-color: rgba(255,255,255,0.1);
+  border-radius: 3rem;
+}
+input[type="text"]:hover {
+  background-color: rgba(255,255,255,0.15);
+}
+input[type="text"]:focus {
+  outline: none;
+}
+
+button {
+  cursor: pointer;
+  padding: 0.5rem;
+}
+button:hover {
+  color: var(--accent-color)
+}
+button:focus {
+  outline: none;
+}
+
+img {
+  max-width: 100%;
+}
+
+svg {
+  fill: none;
+}
+
+::selection {
+	background: var(--accent-color);
+	color: var(--background-color);
+}
+
+
+
+
+/*------------------------------------*\
+    Card layout
+\*------------------------------------*/
+
+.card {
+  padding: 1rem;
+  background-color: #1A1A1A;
+  border-radius: 1rem;
+  display: flex;
+  align-items: center;
+  min-height: 420px;
+}
+.left {
+  width: 50%;
+  padding: 1rem;
+  text-align: center;
+}
+.right {
+  width: 50%;
+}
+@media (max-width: 768px) {
+  .card {
+    border-radius: 0;
+    flex-direction: column;
+  }
+  .left,
+  .right {
+    width: 100%;
+  }
+}
+
 </style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,144 @@
+<template>
+  <div class="todo-list">
+    <h3>To-do items</h3>
+    <form class="new-item-form" v-on:submit.prevent="addItem" method="post">
+      <input type="text" placeholder="Type something here..." v-model="inputValue" required />
+      <button type="submit">
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-plus"><line x1="12" y1="5" x2="12" y2="19"></line><line x1="5" y1="12" x2="19" y2="12"></line></svg>
+      </button>
+    </form>
+    <h4>{{items.length}} items</h4>
+    <ul class="list">
+      <li class="item" v-for="(item, index) in items" :key="index">
+
+        <form v-if="item.editing" class="inner" method="post" v-on:submit.prevent="toggleItemEditing(item)">
+          <input type="text" v-model="items[index].title" required />
+          <button class="confirm">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-check"><polyline points="20 6 9 17 4 12"></polyline></svg>
+          </button>
+        </form>
+
+        <div v-else v-bind:class="{inner: true, done: item.done}">
+          <button class="check" v-on:click="toggleItemStatus(item)">
+            <svg v-if="item.done" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-check-circle"><path d="M22 11.08V12a10 10 0 1 1-5.93-9.14"></path><polyline points="22 4 12 14.01 9 11.01"></polyline></svg>
+            <svg v-else xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-circle"><circle cx="12" cy="12" r="10"></circle></svg>
+          </button>
+          <div class="name" v-on:click="toggleItemEditing(item)">
+            <strike v-if="item.done">{{item.title}}</strike>
+            <span v-else>{{item.title}}</span>
+          </div>
+          <button class="delete" v-on:click="deleteItem(item)">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-x"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg>
+          </button>
+        </div>
+      </li>
+    </ul>
+    <div class="info">
+      <span>Click to edit.</span>
+      <a v-on:click="clearCheckedItems">Clear checked items</a>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'TodoList',
+  data: () => ({
+    inputValue: '',
+    items: []
+  }),
+  methods: {
+    addItem() {
+      event.preventDefault()
+      this.items.push({
+        title: this.inputValue,
+        done: false,
+        editing: false
+      })
+      this.inputValue = ''
+    },
+    toggleItemStatus(item) {
+      item.done = !item.done
+    },
+    toggleItemEditing(itemToToggle) {
+      event.preventDefault()
+      this.items = this.items.map(item =>
+        item === itemToToggle
+          ? ({...item, editing: !item.editing})
+          : ({...item, editing: false})
+      )
+    },
+    deleteItem(itemToDelete) {
+      this.items = this.items.filter(item => item !== itemToDelete)
+    },
+    clearCheckedItems() {
+      this.items = this.items.filter(item => !item.done)
+    },
+  }
+}
+</script>
+
+<style scoped>
+
+.todo-list {
+  display: flex;
+  flex-direction: column;
+  align-items: stretch;
+  padding: 1rem;
+  border-radius: 1rem;
+  background-color: #212121;
+  min-height: 320px;
+}
+
+.info {
+  padding: 1rem;
+  margin: auto 0 0 0;
+  font-size: 0.67rem;
+  opacity: 0.67;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+
+.new-item-form {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+
+h4 {
+  padding: 0 0.5rem;
+}
+
+.list {
+  list-style: none;
+  padding: 0;
+  margin-top: 0;
+}
+
+.item {
+  overflow: hidden;
+  border-radius: 3rem;
+}
+.item:hover {
+  background-color: rgba(255,255,255,0.05);
+}
+
+.inner {
+  display: flex;
+  align-items: stretch;
+}
+.inner.done {
+  opacity: 0.5;
+}
+
+.name {
+  display: flex;
+  align-items: center;
+  width: 100%;
+  padding: 0 0.5rem;
+  cursor: text;
+}
+
+</style>
